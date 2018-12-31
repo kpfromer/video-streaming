730692f55c605e14de9bd34299e43877dd3b56e5
Kyle Pfromer <kpfromer@users.noreply.github.com>
2018-12-30 21:07:23 -0700

    Update backend for buffer player

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,8 +9,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/example.webm', (req, res) => {
-  return res.sendFile(path.join(__dirname, '../videos', 'example.webm'))
-})
+app.use('/', express.static(path.join(__dirname, '..', 'public')))
+
+// app.get('/example.webm', (req, res) => {
+//   return res.sendFile(path.join(__dirname, '../videos', 'example.webm'))
+// });
+
+// app.get('/example.json', (req, res) => {
+//   return res.sendFile(path.join(__dirname, '../data', 'example.json'))
+// })
 
 app.listen(3001);
